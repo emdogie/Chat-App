ce7581fd89e333fa62693ec26967f253ee37157d
Marek <marek.garczewski@gmail.com>
2019-08-15 22:46:20 +0200

    Initial Commit

---
+++ Chat App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chat App
+//
+//  Created by Marek Garczewski on 15/08/2019.
+//  Copyright © 2019 Marek Garczewski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
